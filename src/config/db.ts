0c470248313987281dd02170f733874707ab8c46
idrissa murenga <1mulengaidriss@gmail.com>
2025-10-11 03:35:08 +0200

    db changed

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,14 +4,14 @@ import type { Express } from 'express';
 
 const connectDB = (app: Express) => {
   try {
-    const PORT = configEnv.PORT;
+    const PORT = Number(configEnv.PORT);
     //connect server to mongodb
     mongoose
       .connect(configEnv.MONGODB_URI as string)
       .then(() => {
         console.log('MongoDB connected');
         //starting and listening to the server
-        app.listen(PORT, () => console.log(`Server running on port ${PORT}....`));
+        app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}....`));
       })
       .catch((error) => {
         console.error('MongoDB connection error:', error);
