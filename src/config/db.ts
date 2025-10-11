9771f44713c91758e9e0eb57fc6b48a2efea6104
idrissa murenga <1mulengaidriss@gmail.com>
2025-10-11 02:55:02 +0200

    changed port on db file

--- src/config/db.ts
+++ src/config/db.ts
@@ -4,15 +4,14 @@ import type { Express } from 'express';
 
 const connectDB = (app: Express) => {
   try {
+    const PORT = configEnv.PORT;
     //connect server to mongodb
     mongoose
       .connect(configEnv.MONGODB_URI as string)
       .then(() => {
         console.log('MongoDB connected');
         //starting and listening to the server
-        app.listen(configEnv.PORT, () =>
-          console.log(`Server running on port ${configEnv.PORT}....`)
-        );
+        app.listen(PORT, () => console.log(`Server running on port ${PORT}....`));
       })
       .catch((error) => {
         console.error('MongoDB connection error:', error);
